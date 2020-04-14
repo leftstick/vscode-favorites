a23de37b0a6a93cfb9be9d330f00ec300912e50e
Howard.Zuo <leftstick@qq.com>
2020-04-14 17:44:30 +0800

    refactor: use latest version of vscode-nls-dev

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,85 @@
-const gulp = require('gulp');
-const rename = require('gulp-rename');
-const fs = require('fs');
-const path = require('path');
-const nls = require('vscode-nls-dev');
-const del = require('del');
+const gulp = require('gulp')
+const path = require('path')
 
-const vscodeLanguages = ['zh-cn', 'zh-tw']; // languages an extension has to be translated to
+const ts = require('gulp-typescript')
+const typescript = require('typescript')
+const sourcemaps = require('gulp-sourcemaps')
+const del = require('del')
+const runSequence = require('run-sequence')
+const es = require('event-stream')
+const vsce = require('vsce')
+const nls = require('vscode-nls-dev')
 
-const transifexApiHostname = 'www.transifex.com';
-const transifexApiName = 'api';
-const transifexApiToken = process.env.TOKEN; // token to talk to Transifex (to obtain it see https://docs.transifex.com/api/introduction#authentication)
-const transifexProjectName = 'vscode-favorites'; // your project name in Transifex
-const transifexExtensionName = 'favorites'; // your resource name in Transifex
+const tsProject = ts.createProject('./tsconfig.json', { typescript })
 
+const inlineMap = true
+const inlineSource = false
+const outDest = 'out'
 
-const iso639_3_to_2 = {chs: 'zh-cn', cht: 'zh-tw'};
+// If all VS Code langaues are support you can use nls.coreLanguages
+const languages = [{ folderName: 'zh-cn', id: 'zh-cn' }]
 
-gulp.task('clean', function(cb) {
-    return del(['i18n', 'favorites-localization'], cb);;
-});
+const cleanTask = function () {
+  return del(['out/**', 'package.nls.*.json', '*.vsix'])
+}
 
-gulp.task('transifex-push', ['clean'], function() {
-    return gulp.src('**/*.nls.json')
-        .pipe(nls.prepareXlfFiles(transifexProjectName, transifexExtensionName))
-        .pipe(nls.pushXlfFiles(transifexApiHostname, transifexApiName, transifexApiToken));
-});
+const internalCompileTask = function () {
+  return doCompile(false)
+}
 
-gulp.task('transifex-pull', ['transifex-push'], function() {
-    return nls.pullXlfFiles(transifexApiHostname, transifexApiName, transifexApiToken, vscodeLanguages, [{
-        name: transifexExtensionName,
-        project: transifexProjectName
-    }])
-        .pipe(gulp.dest(`./${transifexExtensionName}-localization`));
-});
+const internalNlsCompileTask = function () {
+  return doCompile(true)
+}
 
-gulp.task('i18n-import', ['transifex-pull'], function() {
-    return gulp.src(`./${transifexExtensionName}-localization/**/*.xlf`)
-        .pipe(nls.prepareJsonFiles())
-        .pipe(gulp.dest('./i18n'));
-});
+const addI18nTask = function () {
+  return gulp
+    .src(['package.nls.json'])
+    .pipe(nls.createAdditionalLanguageFiles(languages, 'i18n'))
+    .pipe(gulp.dest('.'))
+}
 
-gulp.task('prepare-package-nls-json', ['i18n-import'], function() {
-    return new Promise(function(res) {
-        fs.readdir('./i18n/', function(err, languages) {
-            Promise
-                .all(languages.map(language => {
-                    return new Promise(resolve => {
-                        gulp
-                            .src(path.join('./i18n/', language, 'package.i18n.json'))
-                            .pipe(rename(`package.nls.${iso639_3_to_2[language]}.json`))
-                            .pipe(gulp.dest('.'))
-                            .on('end', resolve);
-                    });
-                }))
-                .then(res);
-        })
-    });
-});
+const buildTask = gulp.series(cleanTask, internalNlsCompileTask, addI18nTask)
 
-gulp.task('default', ['prepare-package-nls-json']);
+const doCompile = function (buildNls) {
+  var r = tsProject
+    .src()
+    .pipe(sourcemaps.init())
+    .pipe(tsProject())
+    .js.pipe(buildNls ? nls.rewriteLocalizeCalls() : es.through())
+    .pipe(buildNls ? nls.createAdditionalLanguageFiles(languages, 'i18n', 'out') : es.through())
+
+  if (inlineMap && inlineSource) {
+    r = r.pipe(sourcemaps.write())
+  } else {
+    r = r.pipe(
+      sourcemaps.write('../out', {
+        // no inlined source
+        includeContent: inlineSource,
+        // Return relative source map root directories per file.
+        sourceRoot: '../src',
+      })
+    )
+  }
+
+  return r.pipe(gulp.dest(outDest))
+}
+
+const vscePublishTask = function () {
+  return vsce.publish()
+}
+
+const vscePackageTask = function () {
+  return vsce.createVSIX()
+}
+
+gulp.task('default', buildTask)
+
+gulp.task('clean', cleanTask)
+
+gulp.task('compile', gulp.series(cleanTask, internalCompileTask))
+
+gulp.task('build', buildTask)
+
+gulp.task('publish', gulp.series(buildTask, vscePublishTask))
+
+gulp.task('package', gulp.series(buildTask, vscePackageTask))
